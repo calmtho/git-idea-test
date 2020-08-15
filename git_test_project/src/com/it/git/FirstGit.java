1af6a225d8e00d763a8dc199a332f326df2d0288
calmtho <calmtho@163.com>
2020-08-15 18:54:51 +0800

    提交git测试工程以及其相关文件到git_idea这个本地仓库中

---
+++ git_test_project/src/com/it/git/FirstGit.java
@@ -0,0 +1,9 @@
+package com.it.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
